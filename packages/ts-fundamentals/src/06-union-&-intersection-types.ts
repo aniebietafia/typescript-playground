29b1a32b85434aa8c304f44808b5f31872e525ae
aniebietafia <aniebietafia87@gmail.com>
2023-12-21 08:11:23 +0100

    union and intersection types

---
+++ packages/ts-fundamentals/src/06-union-&-intersection-types.ts
@@ -0,0 +1,26 @@
+//* Union types in Typescript
+
+const humidity = 79; // literal types
+
+// create types for two sets of numbers
+
+// A set of numbers from 1 to 5
+type OneThroughFive = 1 | 2 | 3 | 4 | 5;
+let lowNumber: OneThroughFive = 3;
+// lowNumber = 8
+
+//? A set of even numbers from 1 to 9
+type Evens = 2 | 4 | 6 | 8;
+let evenNumber: Evens = 2;
+// evenNumber = 5
+
+//? A set of numbers from 1 to 5 OR a set of even numbers from 1 to 9
+let evenOrLowNumber = 5 as Evens | OneThroughFive;
+// let evenOrLowNumber: Evens | OneThroughFive = 5
+
+//? Control flow sometimes results in union types
+function flipCoin(): "heads" | "tails" {
+  if (Math.random() > 0.5) return "heads";
+  return "tails";
+}
+const outcome = flipCoin();
