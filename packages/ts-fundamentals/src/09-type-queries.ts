5571ae7fc2cfe974c922d4ca7c67957710d1f249
aniebietafia <aniebietafia87@gmail.com>
2023-12-21 16:27:25 +0100

    type queries - typeof

--- packages/ts-fundamentals/src/09-type-queries.ts
+++ packages/ts-fundamentals/src/09-type-queries.ts
@@ -3,3 +3,12 @@ type DatePropertyNames = keyof Date;
 
 type DateStringPropertyNames = DatePropertyNames & string;
 type DateSymbolPropertyNames = DatePropertyNames & symbol;
+
+// typeof
+async function main() {
+  const apiResponse = await Promise.all([
+    fetch("https://example.com"),
+    Promise.resolve("Titanium White"),
+  ]);
+  type ApiResponseType = typeof apiResponse;
+}
