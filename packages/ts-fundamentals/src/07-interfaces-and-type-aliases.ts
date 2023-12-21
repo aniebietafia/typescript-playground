f28bf5a5e8ee5d0c22fe64a7e8ff0d9ada68fd54
aniebietafia <aniebietafia87@gmail.com>
2023-12-21 09:24:12 +0100

    interfaces and type aliases

--- packages/ts-fundamentals/src/07-interfaces-and-type-aliases.ts
+++ packages/ts-fundamentals/src/07-interfaces-and-type-aliases.ts
@@ -18,3 +18,33 @@ const donation = {
 };
 
 printAmount(donation);
+
+function flipCoin(): "heads" | "tails" {
+  if (Math.random() > 0.5) return "heads";
+  return "tails";
+}
+
+const success1 = [
+  "success",
+  { name: "Aniebiet Afia", email: "aniebietafia@gmail.com" },
+] as const;
+const fail1 = ["error", new Error("Something went wrong!")] as const;
+
+function maybeGetUserInfo():
+  | readonly ["error", Error]
+  | readonly ["success", { name: string; email: string }] {
+  if (flipCoin() === "heads") {
+    return success;
+  } else {
+    return fail;
+  }
+}
+
+// Model the return type as a type aliase
+type UserInfoOutcomeError = readonly ["error", Error];
+type UserInfoOutcomeSuccess = readonly [
+  "success",
+  { name: string; email: string }
+];
+
+type UserInfoOutcome = UserInfoOutcomeError | UserInfoOutcomeSuccess;
