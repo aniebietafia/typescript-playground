6daf01b9f466ded12d859edb4224d0d11017d98e
aniebietafia <aniebietafia87@gmail.com>
2023-12-24 16:31:26 +0100

    callables

---
+++ packages/ts-fundamentals/src/10-callables-and-constructables.ts
@@ -0,0 +1,10 @@
+// Callables
+
+interface TwoNumberCalculation {
+  (x: number, y: number): number;
+}
+
+type TwoNumberCalc = (x: number, y: number) => number;
+
+const add: TwoNumberCalculation = (a, b) => a + b;
+const subtract: TwoNumberCalc = (x, y) => x - y;
