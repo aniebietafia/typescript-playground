1aadcbd21eb730ea1b4da0683a384aae13b8001b
aniebietafia <aniebietafia87@gmail.com>
2023-12-24 16:52:31 +0100

    void

--- packages/ts-fundamentals/src/10-callables-and-constructables.ts
+++ packages/ts-fundamentals/src/10-callables-and-constructables.ts
@@ -14,3 +14,14 @@ function printFormattedJSON(obj: string[]) {
   console.log(JSON.stringify(obj, null, " "));
 }
 const xx = printFormattedJSON(["hello", "world"]);
+
+function invokeInFourSeconds(callback: () => undefined) {
+  setTimeout(callback, 4000);
+}
+function invokeInFiveSeconds(callback: () => void) {
+  setTimeout(callback, 5000);
+}
+
+const valuees: number[] = [];
+// invokeInFourSeconds(() => valuees.push(4));
+invokeInFiveSeconds(() => valuees.push(4));
