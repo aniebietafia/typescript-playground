366780ed1b59212f5d476297d073b053dd9ae527
aniebietafia <aniebietafia87@gmail.com>
2023-12-24 16:45:57 +0100

    void

--- packages/ts-fundamentals/src/10-callables-and-constructables.ts
+++ packages/ts-fundamentals/src/10-callables-and-constructables.ts
@@ -8,3 +8,9 @@ type TwoNumberCalc = (x: number, y: number) => number;
 
 const add: TwoNumberCalculation = (a, b) => a + b;
 const subtract: TwoNumberCalc = (x, y) => x - y;
+
+// void
+function printFormattedJSON(obj: string[]) {
+  console.log(JSON.stringify(obj, null, " "));
+}
+const xx = printFormattedJSON(["hello", "world"]);
