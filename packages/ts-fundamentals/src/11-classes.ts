cdf1318170daeb5cbfc526b0c7f01339887d2040
aniebietafia <aniebietafia87@gmail.com>
2023-12-26 12:14:59 +0100

    classes

--- packages/ts-fundamentals/src/11-classes.ts
+++ packages/ts-fundamentals/src/11-classes.ts
@@ -1,14 +1,24 @@
 // Classes
 
 class Car {
+  static nextSerialNumber = 100;
+  static generateNextSerialNumber() {
+    return this.nextSerialNumber++;
+  }
+
   make: string;
   model: string;
   year: number;
+  serialNumber = Car.generateNextSerialNumber();
 
   constructor(make: string, model: string, year: number) {
     this.make = make;
     this.model = model;
     this.year = year;
+  }
+
+  getLabel() {
+    return `${this.make} ${this.model} ${this.year} - #${this.serialNumber}`;
   }
 }
 
